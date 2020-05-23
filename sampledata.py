9f0a653bb163c2a9f89270d0bbf5217070bd8f5c
chengxi <mailper@gmail.com>
2020-05-22 21:23:30 -0700

    remove pyc

--- sampledata.py
+++
@@ -1,9 +0,0 @@
-
-
-common_cropchoices = [('corn', 'Corn'), ('soy', 'Soybean'), ('cotton', 'Cotton'), ('zas', 'zsdfsd')]
-sample_owners = [
-{'uid': 258947, 'name': 'Mr Thomas Brown'},
-{'uid': 326621, 'name': 'Mr Alfie Moore'},
-
-]
-
